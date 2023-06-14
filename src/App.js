4885a7ec739da97a776fb235f93ce134a4e07f1a
unknown <ahmedwoye@gmail.com>
2023-06-14 22:29:59 +0100

    update of changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-
+import "./styles.css";
+import React from 'react';
 import './App.css';
+import Home from './components/pages/Home';
 import Navbar from './components/Navbar';
+import image1 from "./images/image1.jpg";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -8,11 +11,40 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
-           
+        <Route path='/' exact component={Home} />
         </Routes>
+
+        <div className="App">
+      <div class="container">
+        <div class="side">
+        <img alt="facebook" width="100%" src={image1} />
+        </div>
+        <div class="side side-content-center">
+       
+            <p>I create affordable, magical images for your company with me as a model.
+Instagram  | Digital creator | Brand ambassador & influencer | Fashion and Style | Modelling
+Manchester,  UK | & | Upsala, Sweden
+</p>
+
+
+
+ 
+</div>
+
+
+  
+</div>
+      </div>
+
+     
+    
       </Router>
+     
+
+ 
     </>
+
+  
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,63 @@
 import React, {useState} from 'react'
 import './Navbar.css';
+
 import { Link } from 'react-router-dom';
 function Navbar() {
     const [click, setClick] = useState(false);
+    
+
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+     
+    
   return (
     <>
 <nav className='navbar'>
     <div className='navbar-container'>
-<Link to="/" className="navbar-logo">
+<Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
 Self-love is a luxury
-            <i class='fab fa-typo3' />
- </Link>
+
+<i class= 'fab fa-fa-typo3' />
+   </Link>
+ 
  <div className='menu-icon' onClick={handleClick} >
- <i className={click ? 'fas fa-times' : 'fas fa-bars' } />
+ <i className={click ? 'fa fa-times' : "fa fa-bars"} />
+ 
  </div>
+
+ <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+             
+ 
+
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Gallery
+              </Link>
+            </li>
+
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Contact
+              </Link>
+            </li>
+
+            <li className='nav-item'>
+              <Link to='https://instagram.com/clemen_bayo?igshid=NTc4MTIwNjQ2YQ==' className='nav-links' onClick={closeMobileMenu}>
+                <i  class="fa fa-instagram" aria-hidden="true"></i>
+              </Link>
+            </li>
+
+          
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                <i  class="fa fa-facebook-official" aria-hidden="true"></i>
+              </Link>
+            </li>
+             
+
+          </ul>
+         
  </div>
  </nav>
     </>
